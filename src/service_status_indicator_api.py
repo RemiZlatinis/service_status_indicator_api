8bba071c99391f54b8ccade1d48a61395d9439ec
Remi Zlatinis <remizlatinis@gmail.com>
2023-03-29 01:54:13 +0300

    Add default settings

--- src/service_status_indicator_api.py
+++ src/service_status_indicator_api.py
@@ -11,13 +11,19 @@ if not TOKEN:
     error("SERVICE_STATUS_INDICATOR_API_TOKEN environment variable not set")
     exit(1)
 
+DEFAULT_UPDATE_INTERVAL = int(os.environ.get(
+    "SERVICE_STATUS_INDICATOR_DEFAULT_UPDATE_INTERVAL", 60))
+SERVICES_FILE_PATH = '/etc/service-status-indicator-api/services.json'
+
 
 def create_app():
     """Service Status Indicator API"""
     app = Flask(__name__)
 
+    log(f'Default update interval: {DEFAULT_UPDATE_INTERVAL}s')
+    log(f'Services file: {SERVICES_FILE_PATH}')
     log('Initialize checking schedulers')
-    initialize_check_schedulers('services.json', 60)
+    initialize_check_schedulers(SERVICES_FILE_PATH, DEFAULT_UPDATE_INTERVAL)
 
     @app.route('/services')
     def services():
@@ -29,7 +35,7 @@ def create_app():
 
         return jsonify(get_services())
 
-    log('Listening ...')
+    log('API is now listening ...')
     return app
 
 
