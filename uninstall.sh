bf0e0b50154ce0397887a827fad2e90e9b38d514
Remi Zlatinis <remizlatinis@gmail.com>
2023-03-30 11:48:16 +0300

    Check if is installed on uninstall

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check if is installed
+if [ ! -d "/etc/service-status-indicator-api/" ]; then
+    echo "Service Status Indicator API is not installed."
+    exit 0 
+fi
+
+
 # Check for root privileges 
 if [[ $EUID -ne 0 ]]; then
     echo "This script must be run as root" 
